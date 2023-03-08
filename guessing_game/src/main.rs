a23b5c5fe4818e160af9344741e3daf20fcd3dcf
the.slow.one <the0slow0one@gmail.com>
2023-03-08 17:13:11 +0530

    Add game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,39 @@
-use rand;
-use std::cmp;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println();
+    println!("This is a guessing game. I will choose a number in [1..100) and you guess it.");
+    let rand_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        // Get input from the user as string
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read the input");
+
+        // Convert str input to unsigned input
+        let user_input: u32 = match user_input.trim().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("Not a number! Try again");
+                continue;
+            }
+        };
+
+        // Compare user input with choosen number
+        match user_input.cmp(&rand_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("Bingo! You win.");
+                break;
+            }
+        }
+    }
+
+    println!("I choose {rand_number}");
 }
+
+// EOF
